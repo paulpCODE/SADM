aae44bad6006584624d822094756913c12e21883
Paul Pilipenko <paulpilipenko@gmail.com>
2022-05-15 17:40:22 +0300

    Fix application engine

--- main.cpp
+++ main.cpp
@@ -4,39 +4,18 @@
 #include <customplotitem.h>
 #include <QtWidgets>
 
-//class WidgetWithPlot : public QWidget {
-//    Q_OBJECT
-
-//public:
-//    explicit WidgetWithPlot(QObject* parent = nullptr) {}
-
-//    void show() {
-
-//        QWidget::show();
-//    }
-
-//};
-
-
 int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
 
-    QGuiApplication app(argc, argv);
-    QApplication app2(argc, argv);
+    QApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
 
 
-    //WidgetWithPlot plt;
-    //    plt.resize(200, 200);
-    //    plt.show();
 
-//    QWidget* window = new QWidget();
-//    window->resize(200, 200);
-//    window->setWindowTitle("DADAYA");
 
     QCustomPlot* plot = new QCustomPlot();
     QVector<double> x(101), y(101); // initialize with entries 0..100
@@ -57,29 +36,18 @@ int main(int argc, char *argv[])
     plot->replot();
     plot->show();
 
-    //    window.
-    //    window.show();
+
     qmlRegisterType<CustomPlotItem>("CustomPlot", 1, 0, "CustomPlotItem");
-
-    //    QQuickView *view = new QQuickView;
-    //    view->setSource(QUrl::fromLocalFile("main.qml"));
-    //    view->show();
-
-    //    QQuickView view(QUrl("qrc:///qml/main.qml"));
-    //    view.setResizeMode(QQuickView::SizeRootObjectToView);
-    //    view.resize(500, 500);
-    //    view.show();
 
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
+            QApplication::exit(-1);
     }, Qt::QueuedConnection);
     engine.load(url);
 
-    app.exec();
-    return app2.exec();
+    return app.exec();
 }
 
 
